fe09239e0ca8c9799496550cb9014aaac513bff1
ZackSnyder77 <59841784+ZackSnyder77@users.noreply.github.com>
2020-02-21 17:43:37 -0600

    reformatted the output numbering contain 3 decimal places

--- PyPoll/code/main.py
+++ PyPoll/code/main.py
@@ -10,7 +10,7 @@ winner = []
 winner_percentage = 0
 
 csvpath = os.path.join('..', 'Resources', 'election_data.csv')
-output_path = os.path.join('..', 'Resources', 'output.txt')
+output_path = os.path.join('..', 'Resources', 'results.txt')
 
 with open(csvpath, newline='') as csvfile:
 
@@ -18,18 +18,21 @@ with open(csvpath, newline='') as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
+    # print(csvreader)
     
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
     # print(f"CSV Header: {csv_header}")
 
-
+    # tallies the total votes in the dataset
+    #places the candidate votes into a list
+    #places unique candidates into a list
     for row in csvreader:
         votes = votes + 1
         vote_counts.append(row[2])
         if row[2] not in candidates:
             candidates.append(row[2])
+    
     
     print("Election Results")
     print("-------------------------")
@@ -39,7 +42,7 @@ with open(csvpath, newline='') as csvfile:
     #counts the votes for each candidate
     for candidate in candidates:
         vote_percentage = float(format((vote_counts.count(candidate)/votes)*100,'.3f'))
-        print(f"{candidate}: {vote_percentage}% ({vote_counts.count(candidate)})")
+        print(f"{candidate}: {format(vote_percentage,'.3f')}% ({vote_counts.count(candidate)})")
 
         if vote_percentage > winner_percentage:
             winner_percentage = vote_percentage
@@ -48,3 +51,16 @@ with open(csvpath, newline='') as csvfile:
     print("-------------------------")
     print(f"Winner: {winner}")
     print("-------------------------")
+    
+    #takes the console output and places them into a 'Results' txt file
+    with open(output_path, 'w') as txtfile:
+        txtfile.write("-------------------------\n")
+        txtfile.write(F"Total Votes: {votes}\n") 
+        txtfile.write("-------------------------\n")
+        for candidate in candidates: 
+            vote_percentage = float(format((vote_counts.count(candidate)/votes)*100,'.3f'))
+            txtfile.write(f"{candidate}: {format(vote_percentage,'.3f')}% ({vote_counts.count(candidate)})\n")
+        txtfile.write("-------------------------\n")
+        txtfile.write(f"Winner: {winner}\n")
+        txtfile.write("-------------------------\n")
+        txtfile.close()
