31e94dd220c5b2f5f584f100466f14bb6c8e6900
ZackSnyder77 <59841784+ZackSnyder77@users.noreply.github.com>
2020-02-21 14:20:34 -0600

    Wrote code for finding the max delta

--- PyBank/code/main.py
+++ PyBank/code/main.py
@@ -1,8 +1,11 @@
 import os
 import csv
 
-months = 0
+monthscount = 0
+months = []
 total = 0
+maxdelta = 0
+maxdeltamonth = []
 revenues = []
 runningdeltas = []
 averagedeltas = 0
@@ -25,25 +28,33 @@ with open(csvpath, newline='') as csvfile:
     #place the revenues in a string to calc average later
     for row in csvreader:
         revenues.append(int(row[1]))
-        months = months + 1
+        months.append(row[0])
+        monthscount = monthscount + 1
         total = total + int(row[1])
 
     
     #calculates the deltas from the second month to the last
     #then calculates the average of all the deltas
-
     for i in range(len(revenues)):
         if i >0:
             # print(deltas[i]-deltas[i-1])
             runningdeltas.append(revenues[i]-revenues[i-1])
             averagedeltas = sum(runningdeltas) / (len(revenues)-1)
+    
+    for i in range(len(runningdeltas)):
+        if int(runningdeltas[i]) > maxdelta:
+            maxdelta = int(runningdeltas[i])
+            maxdeltamonth = months[i+1]
 
-    # print(f"Sum: {sum(runningdeltas)}")
-
-    # print(len(deltas))
-    print(f"Total Months: {months}")
-    print(f"Total: ${total}")
-    print(f"Average Change: ${round(averagedeltas,2)}")
+    
+    # print(maxdelta)
+    # print(maxdeltamonth)
+    # print(months)
+    
+    # print(f"Total Months: {monthscount}")
+    # print(f"Total: ${total}")
+    # # print(f"Average Change: ${round(averagedeltas,2)}")
+    # print(f"Increase in Profits: {maxdeltamonth} (${maxdelta})")
 
 
 
